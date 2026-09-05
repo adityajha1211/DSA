032927f9323a869ccf3e91cc8c8281690232c861
Aditya Kumar Jha <adityajha847308@gmail.com>
2026-09-05 23:43:12 +0530

    Time: 4 ms (9.92%) | Memory: 44.3 MB (8.53%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String clearDigits(String s) {
+        if(s.length()==0) return s;
+        Stack<Character> ans = new Stack<>();
+        for(int i = 0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(!ans.empty() && Character.isDigit(c)){
+                ans.pop();
+            } else {
+                ans.push(c);
+            }
+        }
+        String jha = "";
+        while(!ans.empty()){
+            jha = jha + ans.pop();
+        }
+        String ok = new StringBuilder(jha).reverse().toString();
+        return ok;
+    }
+}
