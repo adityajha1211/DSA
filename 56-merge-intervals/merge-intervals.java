4c5148744820e85dbf575698a67bac03284f6ed9
Aditya Kumar Jha <adityajha847308@gmail.com>
2026-09-06 21:18:16 +0530

    Time: 7 ms (99.08%) | Memory: 49.2 MB (26.87%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] merge(int[][] arr) {
+        List<int[]> ans = new ArrayList<>();
+        if(arr.length == 0 || arr == null) return ans.toArray(new int[ans.size()][]);
+        Arrays.sort(arr,(a,b)->a[0]-b[0]);
+        int start = arr[0][0];
+        int end = arr[0][1];
+        for(int[] i : arr){
+            if(i[0] <= end) end = Math.max(end,i[1]);
+            else{
+                ans.add(new int[]{start,end});
+                start = i[0];
+                end = i[1];
+            }
+        }
+         ans.add(new int[]{start,end});
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
