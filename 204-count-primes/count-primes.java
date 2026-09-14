dcfc6f0ebec5256d041f71387b1215e089310121
Aditya Kumar Jha <adityajha847308@gmail.com>
2026-09-14 18:03:16 +0530

    Time: 804 ms (5.32%) | Memory: 79.8 MB (20.93%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countPrimes(int n) {
     boolean[] ans = new boolean[n];
-    for(int i = 0;i<n;i++){
+    for(int i = 2;i<n;i++){
         ans[i] = true;
     }
     for(int i = 2;i*i<n;i++){
